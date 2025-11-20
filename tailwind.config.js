445d899f214134630b243e60824c5bb48dc058c7
Deezi <deezitheviper@gmail.com>
2025-11-20 21:00:39 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default  {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        secon: "rgb(0 213 160)",
+      },
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
+      boxShadow: {
+        secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+      },
+    },
+  },
+  plugins: [],
+}
