b21346b1d835441a1c12885e6e1cf614db99e9f0
Deezi <deezitheviper@gmail.com>
2025-11-22 13:35:44 +0100

    mobile bar

---
+++ src/components/Mobilebar.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react'
+import { IconProps } from '../types'
+import {useLocation,Link} from 'react-router-dom'
+
+export const navs = [{
+    name:'Dashboard',
+    navlink:'/',
+    imgUrl: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+  </svg>,
+
+},
+{
+    name:'Deposit',
+    navlink:'deposit',
+    imgUrl: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+  </svg>,
+
+}
+,
+{
+    name:'Withdraw',
+    navlink:'withdraw',
+    imgUrl: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+  </svg>
+  ,
+
+}
+,
+{
+    name:'Profile',
+    navlink:'profile',
+    imgUrl: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+  </svg>
+  
+  ,
+
+}
+]
+
+const Icon: React.FC<IconProps> = ({name,imgUrl,isActive,navlink})  => (
+    <div  className={`  rounded-xl cursor-pointer p-2 ${isActive && isActive === navlink? 'bg-[#242727] text-[#f6b430]' :'text-[#5c6161]' } `} title={`${name}`}>
+   {imgUrl}
+
+</div>
+)
+
+const Mobilebar = () => {
+    
+    const [isActive, setIsActive] = useState<string>('/');
+  
+    const location = useLocation();
+    const { pathname } = location;
+  
+   
+    useEffect(() => {
+      if(pathname === '/') return setIsActive('/')
+      setIsActive(pathname.substring(1))
+    },[pathname])
+  
+  return (
+    <div className='p-6 flex sm:hidden flex-col justify-between items-center  '>
+  
+        <div className='flex flex-row bottom-0 h-max  fixed   gap-2 sm:gap-4 rounded-full px-6  p-2 bg-[#0b0b0b]'>
+
+        {
+            navs.map((item,key) => (
+                <Link to={item.navlink} key={key} onClick={() => {
+                    setIsActive(item.navlink)
+                  }} >
+                <Icon name={item.name}  isActive={isActive} navlink={item.navlink} imgUrl={item.imgUrl} />
+            </Link>
+            ))
+        }
+        </div>
+
+    </div>
+  )
+}
+
+export default Mobilebar
